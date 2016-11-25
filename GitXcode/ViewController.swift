06515e4ab6858acab0a45da4d581758a8726d82c
Anatoly <ant.tsym@gmail.com>
2016-11-25 05:36:16 +0300

    Initial Commit

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by Анатолий Цымбалов on 25.11.16.
+//  Copyright © 2016 Анатолий Цымбалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
